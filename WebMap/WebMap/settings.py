b8cf9343a0e57e315531ba2ead5956bb1e92801e
Ryuuen8 <sabridobryan@gmail.com>
2026-04-29 16:32:39 +0800

    map changes

--- WebMap/WebMap/settings.py
+++ WebMap/WebMap/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Map.apps.MapConfig'
+    'Map.apps.MapConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
